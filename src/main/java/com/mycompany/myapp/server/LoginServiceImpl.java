9da7cae4a6eeb07cedee3565de454b043ab8568e
Temur Uzbekov <TUzbekov@it.ru>
2016-07-12 11:06:42 +0500

    Store login service instance and session params

--- src/main/java/com/mycompany/myapp/server/LoginServiceImpl.java
+++ src/main/java/com/mycompany/myapp/server/LoginServiceImpl.java
@@ -7,27 +7,49 @@ import com.filenet.api.core.ObjectStore;
 import com.filenet.api.util.UserContext;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.mycompany.myapp.client.application.login.LoginService;
+import javax.servlet.ServletException;
 
 public class LoginServiceImpl extends RemoteServiceServlet implements LoginService {
-    final private String contentEngineServerURI = "http://172.19.215.15:9080/wsi/FNCEWS40MTOM/";
-    final private String objectStoreID = "TAD";
-    private static ObjectStore objectStore;
+    final private static String contentEngineServerURI = "http://172.19.215.15:9080/wsi/FNCEWS40MTOM/";
+    final private static String objectStoreID = "TAD";
+    private static LoginServiceImpl instance;
+    private Connection connection;
+    private String login;
+    private String password;
+    private ObjectStore objectStore;
 
-    public static ObjectStore getObjectStore() {
-        return objectStore;
+    public static LoginServiceImpl getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        instance = this;
     }
 
     public void loginUser(String login, String password) {
         Connection connection = Factory.Connection.getConnection(contentEngineServerURI);
-        UserContext.get().pushSubject(
-                UserContext.createSubject(connection, login, password, null) //IDEA complains on direct use of Subject
-        );
+        this.login = login;
+        this.password = password;
+        this.connection = connection;
+        pushSubject();
         try {
             Domain domain = Factory.Domain.getInstance(connection, null);
             ObjectStore objectStore = Factory.ObjectStore.fetchInstance(domain, objectStoreID, null);
-            LoginServiceImpl.objectStore = objectStore;
+            this.objectStore = objectStore;
         } finally {
-            UserContext.get().popSubject();
+            popSubject();
         }
+    }
+
+    public void pushSubject() {
+        UserContext.get().pushSubject(
+                UserContext.createSubject(connection, login, password, null) //IDEA complains about direct use of Subject
+        );
+    }
+
+    public void popSubject() {
+        UserContext.get().popSubject();
     }
 }
